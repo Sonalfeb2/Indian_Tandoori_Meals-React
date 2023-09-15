cb151792750cfc1f7e68d0841d380a3ab17100a3
Sonal Agrawal <sonalfeb7@gmail.com>
2023-09-15 11:24:19 +0530

    Add Meal Summary

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,14 @@
-import Header from "./Layout/Header"
-const Home = () =>{
-    return (
-        <Header/>
-    )
-}
+import { Fragment } from "react";
+import Header from "./Layout/Header";
+import Meals from "./Meals/Meals";
+const Home = () => {
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
+};
 export default Home;
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,20 @@
+import { Fragment } from "react";
 import "./Header.css";
 import CartBtn from "./HeaderCartButton";
+import MealImg from "../../assets/headerMeal.jpg";
 const Header = () => {
   return (
-    <div className="header">
-      <div className="logo">Indian Tandoori Meals</div>
-      <div className="header-right">
-        <CartBtn />
+    <Fragment>
+      <header>
+        <div className="logo">Indian Tandoori Meals</div>
+        <div className="header-right">
+          <CartBtn />
+        </div>
+      </header>
+      <div className="main-image">
+        <img src={MealImg} alt="A table full of delecious food" />
       </div>
-    </div>
+    </Fragment>
   );
 };
 export default Header;
